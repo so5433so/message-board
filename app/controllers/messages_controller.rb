3b7849ca0672a63c715c0f57a996d951a07dda8d
So Okumura <so_5433_so@yahoo.co.jp>
2016-09-21 03:59:47 +0000

    creat message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @message = Message.all
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_too root_path , notice: 'メッセージを保存しました'
   end
 end
