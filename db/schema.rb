7d857e785a45673c3323949a36d080d058c42c4a
So Okumura <so_5433_so@yahoo.co.jp>
2016-09-24 06:59:01 +0000

    challenge2

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921020523) do
+ActiveRecord::Schema.define(version: 20160924053056) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
